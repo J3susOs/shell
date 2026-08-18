1ad908231f12f367bb53de5d76a20a1e528aa7e4
J3susOs <j3sus.osornio@gmail.com>
2026-08-18 17:56:28 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
          System.out.print("$ ");
+
+        Scanner input = new Scanner(System.in);
+
+        String comandos = input.nextLine();
+
+        System.out.println( comandos + ", command not found ");
+
+        input.close();
     }
 }
