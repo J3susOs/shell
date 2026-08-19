5ffd058ee4367d6165a64eca03fd243e28e7621b
J3susOs <j3sus.osornio@gmail.com>
2026-08-18 19:13:06 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
             System.out.print("$ ");
             String command = scanner.nextLine();
             System.out.println(command + ": command not found");
+            System.out.println("$ exit");
             System.exit(1);
         }
 
