32a1719f437382d57ce01d100da670c22e60440a
J3susOs <j3sus.osornio@gmail.com>
2026-08-18 18:08:32 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+
 
         // Captures the user's command in the "command" variable
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+        //String command = scanner.nextLine();
 
         // Prints the "<command>: command not found" message
-        System.out.println(command + ": command not found");
+        //System.out.println(command + ": command not found");
+
+        int i = 1;
+        while (i > 0) {
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+            System.out.println(command + ": command not found");
+        }
 
     }
 }
